ad9cf1a45d9e0071336ab11c6af9fd9e74776c7c
notyeat <sialga@qq.com>
2018-04-23 14:38:05 +0800

    second commit

--- YokiCTF/urls.py
+++ YokiCTF/urls.py
@@ -21,5 +21,6 @@ from index import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
+    path('base/',views.baseNavbar),
     path('subject',include(urls))
 ]
--- index/views.py
+++ index/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render,get_object_or_404
 from .models import subject,subjectType,subjectPackage
 
 # Create your views here.
+
+Navbar = subjectType.objects.all().values("type_name")
+
 def index(request):
     context = {}
     context['packageInfos'] = subjectPackage.objects.all()
-    print(f"------------{context}-----------")
+    context['subjectType'] = Navbar
     return render(request,'index.html',context)
 
 def selectSubjects(request,subject_id):
     subjectObject = get_object_or_404(subject,pk=subject_id)
-    return render(request,"subject.html",context={"subjectObject":subjectObject})
+    return render(request,"subject.html",context={"subjectObject":subjectObject,"subjectType":Navbar})
+
+def baseNavbar(request):
+    return render(request,"base.html",context={"subjectType":Navbar})
