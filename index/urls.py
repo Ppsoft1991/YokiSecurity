cb5d67d8da90dda04639468e120d23a7ad57aeeb
notyeat <774435240@qq.com>
2018-04-23 21:07:31 +0800

    再次美化前端

--- index/urls.py
+++ index/urls.py
@@ -3,5 +3,5 @@ from .views import selectSubjects,typeDisplay
 
 urlpatterns = [
     path('/<int:subject_id>',selectSubjects,name="subject_id"),
-    path('/<type>',typeDisplay,name="type")
+    path('/<type>',typeDisplay,name="type"),
 ]
